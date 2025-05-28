b00b6a82920f2e52fdb8e53768d84757b4c6b77f
dansilber08 <48389991@est.ort.edu.ar>
2025-05-27 22:51:18 -0300

    probar si funciona

---
+++ src/assets/components/home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+    <div>
+        <h1>Pantalla Principal</h1>
+        <button>Transferir</button>
+        <button>Solicitar</button>
+    </div>
+    )
+}
+export default Home
