e786c70a15071f0ed1abe05ffe58902d6f3757a6
dansilber08 <48389991@est.ort.edu.ar>
2025-06-25 11:51:50 -0300

    barra confirmar

--- src/components/transferir.jsx
+++ src/components/transferir.jsx
@@ -13,12 +13,48 @@ function Transferir() {
 
   const handleConfirmar = () => {
     if (monto) {
-      setConfirmacion(`Transferencia realizada de $${monto}`);
+       setConfirmacion(`Transferencia realizada de $${monto}`);
       setMonto('');
-    } else {
-      setConfirmacion('Por favor ingresa un monto');
+   } else {
+       setConfirmacion('Por favor ingresa un monto');
     }
   };
+  const [confirmado, setConfirmado] = useState(false);
+
+  const [sliderX, setSliderX] = useState(0);
+const maxSlide = 200; // tamaño máximo de desplazamiento
+
+const handleDrag = (e) => {
+  if (confirmado) return;
+
+  const newX = e.clientX - e.currentTarget.parentElement.getBoundingClientRect().left;
+  if (newX >= 0 && newX <= maxSlide) {
+    setSliderX(newX);
+  }
+};
+
+
+const handleDragEnd = () => {
+  if (sliderX > maxSlide * 0.9) {
+    if (monto) {
+      handleConfirmar();
+      setSliderX(maxSlide); // Lo pone al final
+
+      // Espera 2 segundos y vuelve al inicio
+      setTimeout(() => {
+        setSliderX(0);
+      }, 2000);
+    } else {
+      setConfirmacion('Por favor ingresa un monto');
+      setSliderX(0); // vuelve al inicio
+    }
+  } else {
+    setSliderX(0); // vuelve al inicio si no llegó al final
+  }
+};
+
+
+
 
   const montosPredefinidos = [10, 20, 50, 100];
 
@@ -26,18 +62,6 @@ function Transferir() {
     <div className="container">
     <button className="btn-volver" onClick={() => navigate('/')}>Volver</button>
       <h1>Transferir</h1>
-      <div>
-        <p>Elige un monto:</p>
-        {montosPredefinidos.map((valor) => (
-          <button
-            key={valor}
-            onClick={() => setMonto(valor)}
-            className="boton-monto"
-          >
-            ${valor}
-          </button>
-        ))}
-      </div>
       <input
         type="number"
         placeholder="Ingresa monto"
@@ -45,9 +69,26 @@ function Transferir() {
         onChange={handleChange}
         className="input-monto"
       />
-      <button onClick={handleConfirmar} className="boton-confirmar">
+      {/* <button onClick={handleConfirmar} className="boton-confirmar">
         Confirmar transferencia
-      </button>
+      </button> */}
+     <div className="slide-container">
+  <div 
+    className="slide-fill" 
+    style={{ width: `${sliderX + 25}px` }} // 25 es la mitad del botón
+  ></div>
+  
+  <div 
+    className="slide-button" 
+    draggable 
+    onDragStart={(e) => e.dataTransfer.setDragImage(new Image(), 0, 0)}
+    onDrag={(e) => handleDrag(e)}
+    onDragEnd={handleDragEnd}
+    style={{ left: `${sliderX}px` }}
+  />
+</div>
+
+
       <p>{confirmacion}</p>
     </div>
   );
